ce6bf07c42d99cbc029736c7e2917f561a537fcf
MrHumine <pubh@interia.pl>
2023-12-08 18:20:50 +0000

    Added DrinkCotrollerTest

---
+++ backend/src/test/java/upt/cafetaria/backend/domain/repository/DrinkControllerTest.java
@@ -0,0 +1,19 @@
+package upt.cafetaria.backend.domain.repository;
+
+import org.junit.Test;
+import upt.cafetaria.backend.model.web.DrinkDto;
+
+public class DrinkControllerTest {
+
+    @Test
+    public void updateDrinkTest_ThatReturnUpdatedDrinks(){
+
+        //Given
+        DrinkDto newDrink;
+        Long id;
+        //When
+
+        //Then
+
+    };
+}

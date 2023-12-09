ec7ad5bc87ac8732d6f2d5093587884255aef49c
MrHumine <pubh@interia.pl>
2023-12-09 11:21:09 +0000

    Add one change

--- backend/src/test/java/upt/cafetaria/backend/domain/model/DessertServiceTest.java
+++ backend/src/test/java/upt/cafetaria/backend/domain/model/DessertServiceTest.java
@@ -1,9 +1,11 @@
 package upt.cafetaria.backend.domain.model;
 
+import org.junit.jupiter.api.Test;
 import upt.cafetaria.backend.model.web.DessertDto;
 
 public class DessertServiceTest {
 
+    @Test
     public void dessertService_IfReturnDessertRepositorySaveDessert(){
         //Given
         DessertDto dto;

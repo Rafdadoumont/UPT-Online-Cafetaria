b41efd49e81e9487bd1a9d5a480cb5b0a21b6f70
MrHumine <pubh@interia.pl>
2023-12-21 10:14:22 +0000

    Add coment in Dessert

--- backend/src/main/java/upt/cafetaria/backend/model/domain/Dessert.java
+++ backend/src/main/java/upt/cafetaria/backend/model/domain/Dessert.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
 
+//Defining this class as an entity to map it to database
 @Entity
 @NoArgsConstructor
 public class Dessert extends Product {}

8467588a20bac43c43b2789997c438a0be0c24ee
Rafdadoumont <72247088+Rafdadoumont@users.noreply.github.com>
2023-12-21 23:45:57 +0000

    Qr code for reservation in dialog

--- frontend/src/app/(user)/reservation/all/components/reservation-table.tsx
+++ frontend/src/app/(user)/reservation/all/components/reservation-table.tsx
@@ -1,46 +1,80 @@
-import React from "react";
-import {Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
-import {Reservation} from "@/types";
-import {Label} from "@/components/ui/label";
-import {format} from "date-fns";
+import React, { useState } from "react";
+import { Table, TableBody, TableCell, TableHeader, TableHead, TableRow } from "@/components/ui/table";
+import { Reservation } from "@/types";
+import { Label } from "@/components/ui/label";
+import QRCode from "react-qr-code";
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from "@/components/ui/dialog";
 
-interface reservationTableProps {
+interface ReservationTableProps {
     reservations: Reservation[]
 }
 
-export function ReservationTable({reservations}: reservationTableProps) {
+export function ReservationTable({ reservations }: ReservationTableProps) {
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [selectedReservation, setSelectedReservation] = useState<Reservation | null>(null);
+
+    const openDialog = (reservation: Reservation) => {
+        setSelectedReservation(reservation);
+        setIsDialogOpen(true);
+    };
+
+    const closeDialog = () => {
+        setSelectedReservation(null);
+        setIsDialogOpen(false);
+    };
+
     return (
-        <Table className="">
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Date</TableHead>
-                    <TableHead>Time</TableHead>
-                    <TableHead>Products</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {reservations && reservations.map((reservation) => (
+        <>
+            <Table>
+                <TableHeader>
                     <TableRow>
-                        <TableCell>{format(new Date(reservation.reservationDate), 'MMMM dd, yyyy')}</TableCell>
-                        <TableCell>{reservation.reservationTime}</TableCell>
-                        <TableCell colSpan={3}>
-                            {reservation.products && reservation.products.map((product, index) => (
-                                <span key={product.productId}>
-                                    {product.name}
-                                    {index !== reservation.products.length - 1 && ', '}
-                                </span>
-                            ))}
-                        </TableCell>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Time</TableHead>
+                        <TableHead>Products</TableHead>
+                        <TableHead />
                     </TableRow>
-                ))}
-                {reservations.length === 0 && (
-                    <TableRow>
-                        <TableCell colSpan={3} className="text-center">
-                            <Label>No reservations</Label>
-                        </TableCell>
-                    </TableRow>
-                )}
-            </TableBody>
-        </Table>
-    )
+                </TableHeader>
+                <TableBody>
+                    {reservations.map((reservation) => (
+                        <TableRow key={reservation.reservationId}>
+                            <TableCell>{reservation.reservationDate}</TableCell>
+                            <TableCell>{reservation.reservationTime}</TableCell>
+                            <TableCell>
+                                {reservation.products?.map((product) => (
+                                    <span key={product.productId}>{product.name}, </span>
+                                ))}
+                            </TableCell>
+                            <TableCell>
+                                <button onClick={() => openDialog(reservation)}>View details</button>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                    {reservations.length === 0 && (
+                        <TableRow>
+                            <TableCell colSpan={4} className="text-center">
+                                <Label>No reservations</Label>
+                            </TableCell>
+                        </TableRow>
+                    )}
+                </TableBody>
+            </Table>
+
+            <Dialog open={isDialogOpen} onOpenChange={closeDialog} >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Reservation Details</DialogTitle>
+                    </DialogHeader>
+                    <p>Reservation Date: {selectedReservation?.reservationDate}</p>
+                    <p>Reservation Time: {selectedReservation?.reservationTime}</p>
+                    <QRCode value={"http://localhost:3000/reservation/" + selectedReservation?.reservationId}/>
+                </DialogContent>
+            </Dialog>
+        </>
+    );
 }
